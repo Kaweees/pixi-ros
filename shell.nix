3da29144dda46ec0b6b60905638b3b99026ac968
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-09-12 23:37:35 -0700

    Init

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python311 # Python 3.11
+    pixi # Python package manager
+    nixfmt # Nix formatter
+    just # Just
+  ];
+
+  # Shell hook to set up environment
+  shellHook = ''
+  '';
+}
